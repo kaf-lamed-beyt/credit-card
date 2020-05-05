b5cbf23c1f52662e0e8ffafa60b4f4c0b8503ff6
Caleb335 <belac335@gmail.com>
2020-05-05 03:32:31 +0100

    setup base styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from './Card'
 
 class App extends React.Component {
     render() {
         return(
             <div>
+                <Card />
                 <h1>Hello dutches</h1>
             </div>
         )
