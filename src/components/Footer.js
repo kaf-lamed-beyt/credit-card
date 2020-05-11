551235f8cb87f32a91f49e48941fcec3c7a72ad8
Caleb335 <belac335@gmail.com>
2020-05-11 12:22:11 +0100

    fixed responsiveness

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer () {
+    return (
+        <div className="footer">
+            <p className="footer__text">Created by <a href="https://sevens-bombsite.netlify.app">seven</a></p>
+        </div>
+    )
+}
+
